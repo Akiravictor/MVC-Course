c50289893f103dac6d8fbe562ba77d5afdfccb85
Akira Hassuda <victor.akira.hs@gmail.com>
2017-07-17 01:52:46 -0300

    Initial Commit

---
+++ AkiraMovies/AkiraMovies/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AkiraMovies.Models;
+
+namespace AkiraMovies.ViewModels
+{
+	public class RandomMovieViewModel
+	{
+		public MovieModels Movie { get; set; }
+		public List<CustomerModels> Customers { get; set; }
+	}
+}
