3d0f4f1a35ab55a959bd2e7b2bf586f9acd0cf67
Akira Hassuda <victor.akira.hs@gmail.com>
2017-07-18 00:43:12 -0300

    Section 2 - Exercise

---
+++ AkiraMovies/AkiraMovies/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AkiraMovies.Models;
+
+namespace AkiraMovies.Controllers
+{
+	public class CustomersController : Controller
+	{
+		// GET: Customer
+		public ActionResult Index()
+		{
+			var customers = GetCustomers();
+
+			return View(customers);
+		}
+
+		public ActionResult Details(int id)
+		{
+			var customerDetail = GetCustomers().FirstOrDefault(a => a.Id == id);
+
+			return View(customerDetail);
+		}
+
+		[Route(@"customers/details/{id}")]
+		private IEnumerable<CustomerModels> GetCustomers()
+		{
+			return new List<CustomerModels>()
+			{
+				new CustomerModels(){Id = 1, Name = "Akira Hassuda" },
+				new CustomerModels(){Id = 2, Name = "Kyoshi Hassuda"}
+			};
+		}
+	}
+}
---
+++ AkiraMovies/AkiraMovies/Controllers/MoviesController.cs
@@ -0,0 +1,70 @@
+﻿using AkiraMovies.Models;
+using AkiraMovies.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AkiraMovies.Controllers
+{
+	public class MoviesController : Controller
+	{
+		// GET: Movie/Random
+		public ActionResult Random()
+		{
+			var movie = new MovieModels() { Name = "Star Wars" };
+			var customers = new List<CustomerModels>
+			{
+				new CustomerModels {Name = "Akira"},
+				new CustomerModels {Name = "Vig"}
+			};
+
+			var viewModel = new RandomMovieViewModel()
+			{
+				Movie = movie,
+				Customers = customers
+			};
+
+			return View(viewModel);
+		}
+
+		public ActionResult Edit(int id)
+		{
+			return Content("id= " + id);
+		}
+
+		public ActionResult Index(int? pageIndex, string sortBy)
+		{
+			//if (!pageIndex.HasValue)
+			//{
+			//	pageIndex = 1;
+			//}
+			//if (String.IsNullOrWhiteSpace(sortBy))
+			//{
+			//	sortBy = "Name";
+			//}
+
+			//var search = String.Format("PageIndex={0}&SortBy={1}", pageIndex, sortBy)
+
+			var movies = GetMovies();
+
+			return View(movies);
+		}
+
+		[Route(@"movie/released/{year:regex(\d{4})}/{month:regex(\d{2}):range(1,12)}")]
+		public ActionResult ByReleaseDate(int year, int month)
+		{
+			return Content(year + "/" + month);
+		}
+
+		private IEnumerable<MovieModels> GetMovies()
+		{
+			return new List<MovieModels>()
+			{
+				new MovieModels{Id = 1, Name = "Star Wars"},
+				new MovieModels{Id = 2, Name = "The Lord of the Rings"}
+			};
+		}
+	}
+}
